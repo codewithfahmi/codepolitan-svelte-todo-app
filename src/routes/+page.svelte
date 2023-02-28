d7f3592650fc9987b6a732c290c39cce3c65d0d2
codewithfahmi <fahmiyahya0@gmail.com>
2023-02-28 19:52:28 +0700

    Membuat Box List Todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Todo App</title>
+</svelte:head>
+
+<div class="container">
+    <header>
+        <h1 class="is-size-3">Todo App</h1>
+    </header>
+    <main>
+        <div class="columns">
+            <div class="column is-4">
+                <div class="card has-background-light">
+                    <div class="card-header">
+                        <p class="card-header-title">Tasks</p>
+                    </div>
+                    <div class="card-content">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>   
+</div>
