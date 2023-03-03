ef9468d771ef9be791a757b76076b9920a63dec7
codewithfahmi <fahmiyahya0@gmail.com>
2023-03-03 23:29:04 +0700

    Membuat CardList Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,8 @@
 <script>
-    import TodoCard from "./TodoCard.svelte";
-    let cards = [
-        {
-            todo: "Belajar HTML",
-            list: "tasks",
-        },
-        {
-            todo: "Belajar JS",
-            list: "tasks",
-        },
-        {
-            todo: "Belajar Svelte",
-            list: "tasks",
-        },
-    ];
-
-    $: todo = "";
-    const handleAddCard = () => (cards = [...cards, { todo, list: "tasks" }]);
+    import CardList from "./CardList.svelte";
+    let taskCards = [];
+    let inProgressCards = [];
+    let doneCards = [];
 </script>
 
 <svelte:head>
@@ -24,42 +10,14 @@
 </svelte:head>
 
 <div class="container">
-    <header>
-        <h1 class="is-size-3">Todo App</h1>
+    <header class="py-5">
+        <h1 class="title is-size-3">Todo App</h1>
     </header>
     <main>
-        <div class="columns">
-            <div class="column is-4">
-                <div class="card has-background-light">
-                    <div class="card-header">
-                        <p class="card-header-title">Tasks</p>
-                    </div>
-                    <div class="card-content">
-                        {#each cards as card}
-                            <TodoCard content={card.todo} />
-                        {/each}
-                        <div class="field">
-                            <div class="control">
-                                <input
-                                    type="text"
-                                    class="input is-primary"
-                                    bind:value={todo}
-                                />
-                            </div>
-                        </div>
-                        <div class="field">
-                            <div class="control">
-                                <button
-                                    on:click={() => {
-                                        handleAddCard(), (todo = "");
-                                    }}
-                                    class="button is-primary">Add Card</button
-                                >
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="columns is-variable is-5">
+            <CardList cards={taskCards} listName={"Tasks"} />
+            <CardList cards={inProgressCards} listName={"In Progress"} />
+            <CardList cards={doneCards} listName={"Done"} />
         </div>
     </main>
 </div>
---
+++ src/routes/CardList.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let cards;
+    export let listName;
+
+    import TodoCard from "./TodoCard.svelte";
+
+    $: todo = "";
+    const handleAddCard = () => (cards = [...cards, { todo, list: "tasks" }]);
+</script>
+
+<div class="column is-4">
+    <div class="card has-background-light">
+        <div class="card-header">
+            <p class="card-header-title">{listName}</p>
+        </div>
+        <div class="card-content">
+            {#each cards as card}
+                <TodoCard content={card.todo} />
+            {/each}
+            <section role="form">
+                <div class="field">
+                    <div class="control">
+                        <input
+                            type="text"
+                            class="input is-primary"
+                            bind:value={todo}
+                        />
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <button
+                            on:click={() => {
+                                handleAddCard(), (todo = "");
+                            }}
+                            class="button is-primary">Add Card</button
+                        >
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
